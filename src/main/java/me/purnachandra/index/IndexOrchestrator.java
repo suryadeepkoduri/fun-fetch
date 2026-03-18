968f622020a128ab44a6c9c36513e5d84bc3e3f4
suryadeepkoduri <suryadeepkoduri@gmail.com>
2026-03-18 19:50:39 +0530

    feat(IndexOrchestrator): add logging for indexing process and empty queue handling

--- src/main/java/me/purnachandra/index/IndexOrchestrator.java
+++ src/main/java/me/purnachandra/index/IndexOrchestrator.java
@@ -5,10 +5,15 @@ import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class IndexOrchestrator {
     private final Indexer indexer;
     private final IndexRepository indexRepository;
     Queue<Integer> localQueue = new LinkedList<>();
+
+    private final Logger log = LoggerFactory.getLogger(IndexOrchestrator.class);
 
     public IndexOrchestrator(Indexer indexer, IndexRepository indexRepository) {
         this.indexer = indexer;
@@ -20,12 +25,14 @@ public class IndexOrchestrator {
             if (localQueue.isEmpty()) {
                 List<Integer> batch = indexRepository.getNextPendingBatch(100);
                 if (batch.isEmpty()) {
+                    log.info("No pending pages to index. Exiting...");
                     break;
                 }
                 localQueue.addAll(batch);
             }
 
             int pageId = localQueue.poll();
+            log.info("Indexing pageId: {}", pageId);
             Map<String, Integer> freqs = indexer.index(indexRepository.fetchPageContent(pageId));
 
             indexRepository.addIndex(pageId, freqs);
